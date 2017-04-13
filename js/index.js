ebe29a62bff3c15a65f4b737bc286f22305e4bde
DragonBaby <854152562@qq.com>
2017-04-13 18:26:17 +0800

    tree success

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/**
+ * Created by DragonBaby on 2017/4/13.
+ */
+(function(){
+    var mind={
+        "meta":{
+            "name":"example",
+            "author":"Dragonbaby",
+            "version":"0.1"
+        },
+        "format":"node_tree",
+        "data":{
+            "id":"root","topic":"root",
+            "children":[{
+                "id":"root/第一个","topic":"1_第一个",
+                "children":[{
+                    "id":"root/第一个/第一个","topic":"1_第一个_第一个"
+                },{
+                    "id":"root/第一个/第二个","topic":"1_第一个_第二个"
+                }]
+            },{
+                "id":"root/第二个","topic":"1_第二个",
+                "children":[]
+            },{
+                "id":"root/第三个","topic":"1_第三个",
+                "children":[]
+            },{
+                "id":"root/第四个","topic":"1_第四个",
+                "children":[{
+                    "id":"root/第四个/第一个","topic":"1_第四个_第一个"
+                }]
+            }]
+        }
+    };
+    var options = {
+        container:'jsmind_container',
+        editable:true,
+        theme:'primary'
+    };
+    var jm = new jsMind.show(options,mind);
+
+    jm.set_theme(theme)  //设置主题
+})();
