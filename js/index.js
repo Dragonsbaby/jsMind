31adb1b4259580c77d095387d040fbad98b29cb1
DragonBaby <854152562@qq.com>
2017-04-17 18:35:32 +0800

    “2017-04-17

--- js/index.js
+++ js/index.js
@@ -34,11 +34,29 @@
     };
     var options = {
         container:'jsmind_container',
-        editable:false,
-        theme:'primary'
+        editable:true,
+        theme:'primary',
+        mode:'side',
+        support_html :true,
+        shortcut:{
+            enable:true,        // 是否启用快捷键
+            handles:{},         // 命名的快捷键事件处理器
+            mapping:{           // 快捷键映射
+                addchild   : 45,    // <Insert>
+                addbrother : 13,    // <Enter>
+                editnode   : 113,   // <F2>
+                delnode    : 46,    // <Delete>
+                toggle     : 32,    // <Space>
+                left       : 37,    // <Left>
+                up         : 38,    // <Up>
+                right      : 39,    // <Right>
+                down       : 40,    // <Down>
+            }
+        }
     };
 
-    var jm = new jsMind.show(options,mind);
+    var jm = new jsMind(options);
+    jm.show(mind);
 
     // jm.set_theme(theme);  //设置主题
     // jm.disable_edit()
