f84bece0644f346877caaef1e6433efb577a4184
DragonBaby <854152562@qq.com>
2017-05-11 14:22:37 +0800

    新修改

--- js/index.js
+++ js/index.js
@@ -12,20 +12,20 @@
         "data":{
             "id":"root","topic":"root",
             "children":[{
-                "id":"root/第一个","topic":"1_第一个",
+                "id":"root/第一个","topic":"1_第一个","attr":"class='doc2'",
                 "children":[{
                     "id":"root/第一个/第一个","topic":"1_第一个_第一个"
                 },{
-                    "id":"root/第一个/第二个","topic":"1_第一个_第二个"
+                    "id":"root/第一个/第二个","topic":"1_第一个_第二个","attr":"class='doc2'"
                 }]
             },{
-                "id":"root/第二个","topic":"1_第二个",
+                "id":"root/第二个","topic":"1_第二个","attr":"class='doc2'",
                 "children":[]
             },{
-                "id":"root/第三个","topic":"1_第三个",
+                "id":"root/第三个","topic":"1_第三个","attr":"class='doc2'",
                 "children":[]
             },{
-                "id":"root/第四个","topic":"1_第四个",
+                "id":"root/第四个","topic":"1_第四个","attr":"class='doc2'",
                 "children":[{
                     "id":"root/第四个/第一个","topic":"1_第四个_第一个"
                 }]
@@ -38,6 +38,10 @@
         theme:'primary',
         mode:'side',
         support_html :true,
+        view:{
+            hmargin:10,
+            vmargin:10
+        },
         shortcut:{
             enable:true,        // 是否启用快捷键
             handles:{},         // 命名的快捷键事件处理器
@@ -50,11 +54,12 @@
                 left       : 37,    // <Left>
                 up         : 38,    // <Up>
                 right      : 39,    // <Right>
-                down       : 40,    // <Down>
+                down       : 40    // <Down>
             }
         }
     };
-
+    setAttrFormJson(mind.data);
+    console.log(mind.data);
     var jm = new jsMind(options);
     jm.show(mind);
 
@@ -62,13 +67,47 @@
     // jm.disable_edit()
     document.getElementById("getData").onclick = function () {
         console.log(jm.get_data("node_tree"));
+        jm.selected_node("ge")
     };
 
+    document.getElementById("addNode").onclick = function(){
+        var selected_node = jm.get_selected_node();
+        if(!selected_node) {alert("请选中文件");}
+        var nodeid = jsMind.util.uuid.newid();
+        var topic = '* Node_'+nodeid.substr(0,5)+' *';
+        var node = jm.add_node(selected_node, nodeid, topic);
+    };
+
+    function setAttrFormJson(data) {
+        if(!data) {return;}
+        if(isExist(data.attr)){
+            var fileName = data.topic;
+            var str = ''+
+                    '<fnode title="'+fileName+'" '+data.attr+'>'+fileName+'</fnode>';
+            data.topic = str;
+        }
+        if('children' in data) {
+            var children = data.children;
+            for (var i = 0; i < children.length; i++) {
+                setAttrFormJson(children[i]);
+            }
+        }
+    }
+    function isExist(param) {
+        if(param == null||param == ''||typeof(param) == 'undefined'){
+            return false;
+        }
+        return true;
+    }
+
 })();
+
 
 
 var jsMd = {
     success : function () {
         
     }
-}
+};
+
+
